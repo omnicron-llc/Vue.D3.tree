c99af3a62e3082b1c38d3ccbafd02e0ef0eeb1d2
Kai Moonbourn <kai@imac.lan>
2022-08-18 23:41:30 -0600

    build

--- src/layout/horizontal.js
+++ src/layout/horizontal.js
@@ -6,9 +6,7 @@ function transformNode (x, y) {
 
 export default {
   size (tree, size, margin, {last, first}) {
-		tree.separation(function(a, b) {
-			return 3
-		}).size([size.height - (margin.y * 2), size.width - (margin.x * 2) - (last + first)])
+		tree.size([size.height - (margin.y * 2), 2* (size.width - (margin.x * 2) - (last + first))])
   },
 
   transformNode,
