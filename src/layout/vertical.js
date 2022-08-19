1b4ffbcadf9a90a0d7d6ec41956ca25a1f190065
Kai Moonbourn <kai@imac.lan>
2022-08-18 23:08:36 -0600

    build

--- src/layout/vertical.js
+++ src/layout/vertical.js
@@ -7,7 +7,8 @@ function transformNode (x, y) {
 
 export default {
   size (tree, size, margin, {last}) {
-    //tree.size([size.width - (margin.x * 2), size.height - (margin.y * 2) - last - minMargin])
+    tree.size([size.width - (margin.x * 2), size.height - (margin.y * 2) - last - minMargin])
+		tree.nodeSize(last + minMargin)
   },
 
   transformNode,
